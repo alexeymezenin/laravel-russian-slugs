744a24e77b3f4d22e63b5c14a214c53ca51a8702
Alexey Mezenin <alexeymezenin@gmail.com>
2016-04-15 11:36:06 +0500

    Facade fixed

--- src/config/seoslug.php
+++ src/config/seoslug.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
+    // Spaces will be replaced with this delimiter,
+    // available delimiters are '-' and '_'
+    'delimiter' => '_',
+
     // 1 - '/категория/книги_в_москве'
     // 2 - '/kategoriya/knigi_v_moskve - translit using Yandex rules
     'urlType' => 1,
 
     // Keep capital letters
     'keepCapitals' => false,
-
-    // Spaces will be replaced with this delimiter,
-    // available delimiters are '-' and '_'
-    'delimiter' => '_',
 
     // Name of a table column where slugs will be stored
     'slugColumnName' => 'slug'
