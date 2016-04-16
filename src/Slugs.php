b5a9911b26f11f8d5a147d0951978f24dfbbb103
Alexey Mezenin <alexeymezenin@gmail.com>
2016-04-16 17:45:13 +0500

    PHPDoc

--- src/Slugs.php
+++ src/Slugs.php
@@ -2,16 +2,40 @@
 
 namespace AlexeyMezenin\RussianSeoSlugs;
 
+/**
+ * Class Slugs
+ * @package AlexeyMezenin\RussianSeoSlugs
+ */
 class Slugs
 {
+    /**
+     * @var string URL converted to slug
+     */
     public $slug;
 
+    /**
+     * @var string Delimiter: '-' or '_'
+     */
     private $delimiter;
 
+    /**
+     * @var integer Type of returned URL
+     */
     private $urlType;
 
+    /**
+     * @var boolean Should capitals be kept?
+     */
     private $keepCapitals;
 
+    /**
+     * Slugs constructor.
+     *
+     * @param string $stringToSlug Original string which will be converted to a slug
+     * @param string $delimiter Slug delimiter '-' or '_'
+     * @param int $urlType Type of returned URL: 1 is for URL with russian letters, 2 is for Translit
+     * @param boolean $keepCapitals Should capitals be kept?
+     */
     public function __construct($stringToSlug, $delimiter = null, $urlType = null, $keepCapitals = null)
     {
         $this->slug = $stringToSlug;
@@ -20,7 +44,15 @@ class Slugs
         $this->keepCapitals = is_null($keepCapitals) ? config('seoslug.keepCapitals') : $keepCapitals;
     }
 
-    // Building a slug
+    /**
+     * Builds a slug based on config.
+     *
+     * @param string $stringToSlug Original string which will be converted to a slug
+     * @param string $delimiter Slug delimiter '-' or '_'
+     * @param int $urlType Type of returned URL: 1 is for URL with russian letters, 2 is for Translit
+     * @param boolean $keepCapitals Should capitals be kept?
+     * @return string
+     */
 	public function build($stringToSlug, $delimiter = null, $urlType = null, $keepCapitals = null)
     {
         $this->__construct($stringToSlug, $delimiter, $urlType, $keepCapitals);
@@ -33,19 +65,32 @@ class Slugs
         return $this->slug;
     }
 
-
+    /**
+     * Removes all symbols except russian and latin letters, numbers, '-' and '_' in $this->slug
+     *
+     * @return $this
+     */
     public function removeInappropriateSymbols(){
         $this->slug = preg_replace("/[^0-9а-яёa-z_-]/iu", '', $this->slug);
 
         return $this;
     }
 
+    /**
+     * Replaces spaces with delimiter in $this->slug
+     * @return $this
+     */
     public function replaceSpacesWithDelimiter(){
         $this->slug = str_replace(' ', $this->delimiter, $this->slug);
 
         return $this;
     }
 
+    /**
+     * Converts $this->slug to lowercase when necessary
+     *
+     * @return $this
+     */
     public function toLower(){
         if($this->keepCapitals === false){
             $this->slug = mb_strtolower($this->slug);
@@ -54,8 +99,11 @@ class Slugs
         return $this;
     }
 
-
-    // Converts string to translit using Yandex rules
+    /**
+     * Converts $this->slug to translit using Yandex rules
+     *
+     * @return $this
+     */
     public function toTranslit()
     {
         // if slug doesn't need to be transliterated
