c9d61e27ad028593effa7cb43de8e66cf749d20e
Alexey Mezenin <alexeymezenin@gmail.com>
2016-04-17 12:11:33 +0500

    PHPDoc

--- src/SlugTrait.php
+++ src/SlugTrait.php
@@ -4,17 +4,42 @@ namespace AlexeyMezenin\RussianSeoSlugs;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class SlugTrait
+ *
+ * @package AlexeyMezenin\RussianSeoSlugs
+ */
 trait SlugTrait
 {
 
+	/**
+	 * Finds a model by slug.
+	 *
+	 * @param $slug
+	 * @return \Illuminate\Database\Eloquent\Model|null
+	 */
 	public static function findBySlug($slug){
 		return self::whereSlug($slug)->first();
 	}
 
+	/**
+	 * Query scope for finding a model by slug.
+	 *
+	 * @param $query
+	 * @param $slug
+	 * @return mixed
+	 */
 	public function scopeWhereSlug($query, $slug){
 		return $query->where(config('seoslug.slugColumnName'), $slug);
 	}
 
+	/**
+	 * Creates or recreates slugs in a column.
+	 *
+	 * @param $column Column to work with.
+	 * @param bool $force When true, forces recreation of a slug, even if it exists.
+	 * @return $this
+	 */
 	public function sluggify($column, $force = false)
 	{
 		$slugColumn = config('seoslug.slugColumnName');
--- src/SlugsFacade.php
+++ src/SlugsFacade.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace AlexeyMezenin\RussianSeoSlugs;
- 
+
 use Illuminate\Support\Facades\Facade;
- 
+
+/**
+ * Class SlugsFacade
+ *
+ * @package AlexeyMezenin\RussianSeoSlugs
+ */
 class SlugsFacade extends Facade {
- 
+
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'slug';
